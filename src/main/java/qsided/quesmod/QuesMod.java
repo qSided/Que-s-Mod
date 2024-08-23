78fb64dae0ee3e6cf173e827ca6ca03b5827e615
qSided <55927663+qSided@users.noreply.github.com>
2024-08-23 10:21:46 -0500

    v0.1.0

--- src/main/java/qsided/quesmod/QuesMod.java
+++ src/main/java/qsided/quesmod/QuesMod.java
@@ -4,19 +4,21 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import qsided.quesmod.blocks.QuesBlocks;
+import qsided.quesmod.items.QuesItems;
 
 public class QuesMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("ques-mod");
+	public static final String MOD_ID = "ques-mod";
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		QuesItems.initialize();
+		QuesBlocks.initialize();
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Que's mod loaded!");
 	}
 }
---
+++ src/main/java/qsided/quesmod/blocks/QuesBlocks.java
@@ -0,0 +1,24 @@
+package qsided.quesmod.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import qsided.quesmod.QuesMod;
+
+public class QuesBlocks {
+    public static Block register(Block block, String name, boolean shouldRegisterItem) {
+        Identifier id = Identifier.of(QuesMod.MOD_ID, name);
+        
+        if (shouldRegisterItem) {
+            BlockItem blockItem = new BlockItem(block, new Item.Settings());
+            Registry.register(Registries.ITEM, id, blockItem);
+        }
+        
+        return Registry.register(Registries.BLOCK, id, block);
+    }
+    
+    public static void initialize() {}
+}
